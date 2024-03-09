4314ae9bfb840ce401180a24421c8d8572b154bd
mints <breathmints@protonmail.com>
2024-03-08 20:02:11 -0800

    put needful function in its own library

---
+++ needful/src/lib.rs
@@ -0,0 +1,5 @@
+pub fn do_needful() {
+    println!("Hello, Sage!");
+
+    std::thread::sleep(std::time::Duration::from_secs(1));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-use std::{thread, time::Duration};
+use needful::*;
 
 fn main() {
     loop {
-        do_needful()
+        do_needful();
     }
-}
-
-fn do_needful() {
-    println!("Hello, Sage!");
-
-    thread::sleep(Duration::from_secs(1));
 }
