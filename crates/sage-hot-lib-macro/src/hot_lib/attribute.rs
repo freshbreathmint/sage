7ba4d9a18480d73a903670eab6db970e5570ae9c
freshbreathmint <33765528+freshbreathmint@users.noreply.github.com>
2024-03-13 21:28:02 -0700

    arguments and assignment expressions

--- crates/sage-hot-lib-macro/src/hot_lib/attribute.rs
+++ crates/sage-hot-lib-macro/src/hot_lib/attribute.rs
@@ -1,4 +1,7 @@
-use syn::{punctuated::Punctuated, token::Comma, Expr, ExprPath, Ident, LitInt, Path, Result};
+use syn::{
+    punctuated::Punctuated, spanned::Spanned, token::Comma, Error, Expr, ExprAssign, ExprLit,
+    ExprPath, Ident, Lit, LitInt, Path, Result,
+};
 
 /// Represents the attributes of a hot library.
 pub(crate) struct HotLibAttribute {
@@ -22,8 +25,9 @@ impl syn::parse::Parse for HotLibAttribute {
         // Parse the token stream into a non-empty, comma seperated list of expressions.
         let args = Punctuated::<Expr, Comma>::parse_separated_nonempty(stream)?;
 
-        // Helper function to check if an expression is an identifier.
-        fn expr_is_ident<I>(expr: &Expr, ident: &I) -> bool
+        /// Helper function to check if an expression is an identifier.
+        /// This is used to identify and extract specific attributes from the procedural macro.
+        fn expr_is_ident<I: ?Sized>(expr: &Expr, ident: &I) -> bool
         where
             Ident: PartialEq<I>,
         {
@@ -34,6 +38,71 @@ impl syn::parse::Parse for HotLibAttribute {
             } else {
                 // If not a `Path`, return `false`.
                 false
+            }
+        }
+
+        // Iterate over each argument in the parsed arguments.
+        for arg in args {
+            // Match for arguments that are assignment expressions.
+            match arg {
+                // If the argument is an assignment expression, destructure to get the left and right sides.
+                Expr::Assign(ExprAssign { left, right, .. }) => match *right {
+                    // If the right side is a literal int, and the left side is ident: "file_watch_debounce"
+                    // Update field with the value of the literal integer.
+                    Expr::Lit(ExprLit {
+                        lit: Lit::Int(lit), ..
+                    }) if expr_is_ident(&left, "file_watch_debounce") => {
+                        file_watch_debounce_ms = Some(lit.clone());
+                        continue;
+                    }
+
+                    // If the left side is ident: "dylib", update the field.
+                    expr if expr_is_ident(&left, "dylib") => {
+                        lib_name = Some(expr);
+                        continue;
+                    }
+
+                    // If the left side is ident: "lib_dir", update the field.
+                    expr if expr_is_ident(&left, "lib_dir") => {
+                        lib_dir = Some(expr);
+                        continue;
+                    }
+
+                    // If the left side is ident: "crate", parse the right side as a string literal.
+                    expr if expr_is_ident(&left, "crate") => {
+                        // Get the span of the expression for error reporting.
+                        let span = expr.span().clone();
+
+                        // Nested `match` statements to extract and validate the string literal.
+                        // The outer `match` checks if expression is a literal expression.
+                        let s = match match expr {
+                            Expr::Lit(ExprLit { lit, .. }) => lit,
+                            // If the expression is not a literal expression, return an error.
+                            _ => return Err(Error::new(left.span(), "unexpected expression type")),
+                        } {
+                            // The inner `match` checks if the literal is a string literal.
+                            Lit::Str(s) => s,
+                            // If the literal is not a string literal, return an error.
+                            _ => return Err(Error::new(span, "unexpected expression type")),
+                        };
+
+                        // Parse the string literal as a `Path` and update the field.
+                        crate_name = Some(s.parse::<Path>().clone()?);
+                        continue;
+                    }
+
+                    // If the left side is ident: "loaded_lib_name_template", update the field.
+                    expr if expr_is_ident(&left, "loaded_lib_name_template") => {
+                        loaded_lib_name_template = Some(expr);
+                        continue;
+                    }
+
+                    // If none of the above conditions are met, return an error.
+                    _ => return Err(Error::new(left.span(), "unexpected attribute name")),
+                },
+
+                // If the argument is not an assignment expression, return an error.
+                _ => return Err(Error::new(arg.span(), "unexpected input")),
             }
         }
 
