f427acc37e50f74812bde8264008eb36a1157a63
freshbreathmint <33765528+freshbreathmint@users.noreply.github.com>
2024-03-28 03:51:19 -0700

    add static build tasks

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
+#[cfg(feature = "reload")]
+use hot_lib::*;
+#[cfg(not(feature = "reload"))]
+use lib::*;
+
+#[cfg(feature = "reload")]
 #[sage_hot_lib::hot_lib(dylib = "lib")]
 mod hot_lib {
     hot_functions_from_file!("crates/lib/src/lib.rs");
 }
 
 fn main() {
+    // Debug
+    #[cfg(feature = "reload")]
+    println!("Hot!");
+    #[cfg(not(feature = "reload"))]
+    println!("Static!");
+
+    // Loop
     loop {
-        hot_lib::test();
+        test();
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
